30d3911179ac03b63f690deb8b05c27f9db1d27d
NbN-Raja <xetrinbn66@gmail.com>
2024-04-07 21:37:31 +0545

    Add: Upload router

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get("/upload",require("./router/index.js"))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
